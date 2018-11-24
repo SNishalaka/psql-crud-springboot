35da595b1c8121bd7276047310fe658d6f658d0b
SNishalaka <s.nishalaka97@gmail.com>
2018-11-24 12:05:23 +0530

    added getall() , findById() and remove() functions

--- src/main/java/com/snishalaka/psqlcrud/controller/StudentController.java
+++ src/main/java/com/snishalaka/psqlcrud/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.snishalaka.psqlcrud.controller;
 import com.snishalaka.psqlcrud.dto.StudentDTO;
 import com.snishalaka.psqlcrud.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/student")
@@ -19,4 +18,20 @@ public class StudentController {
     public void save(@RequestBody StudentDTO studentDTO){
         studentService.saveStudentDetails(studentDTO);
     }
+
+    @RequestMapping(value = "/all-details" , method = RequestMethod.GET)
+    public List<StudentDTO> getALl() {
+        return studentService.getALlStudents();
+    }
+
+    @RequestMapping(value = "/find/{sid}" , method = RequestMethod.GET)
+    public StudentDTO getStudent(@PathVariable("sid") String sid) {
+        return studentService.findStudentById(sid);
+    }
+
+    @RequestMapping(value = "dlt/{sid}" , method = RequestMethod.DELETE)
+    public void deleteStdent(@PathVariable("sid") String sid) {
+        studentService.removeStuednt(sid);
+    }
+
 }
--- src/main/java/com/snishalaka/psqlcrud/service/StudentService.java
+++ src/main/java/com/snishalaka/psqlcrud/service/StudentService.java
@@ -6,6 +6,9 @@ import com.snishalaka.psqlcrud.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class StudentService {
 
@@ -21,5 +24,32 @@ public class StudentService {
                 studentDTO.getAddress(),
                 studentDTO.getAge()
         ));
+    }
+
+    //get all student details
+    public List<StudentDTO> getALlStudents(){
+        List<Student> studentsList = (List<Student>) studentRepository.findAll();
+        return studentsList.stream().map(student -> new StudentDTO(
+                student.getSid(),
+                student.getName(),
+                student.getAddress(),
+                student.getAge()
+        )).collect(Collectors.toList());
+    }
+
+    //find by sid
+    public StudentDTO findStudentById(String sid){
+        Student student = studentRepository.findById(sid).get();
+        return new StudentDTO(
+                student.getSid(),
+                student.getName(),
+                student.getAddress(),
+                student.getAge()
+        );
+    }
+
+    //remove student by sid
+    public void removeStuednt(String sid){
+        studentRepository.deleteById(sid);
     }
 }
